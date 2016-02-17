fbe2c1c572218e7c285deafcff0fa71eebfd263e
Jacqueline Kouri <jackie@kourifamily.com>
2016-02-17 09:50:03 -0500

    pushing to github with comment

--- ReminderApp/ViewController.swift
+++ ReminderApp/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    //Trying to push changes to git!
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
